4ab036a3d5c5ad58855b05c07e0ff4d77155aa31
foadtkf <fakhrulif@gmail.com>
2023-10-31 20:05:07 +0600

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-slate-900/80 min-h-screen">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
